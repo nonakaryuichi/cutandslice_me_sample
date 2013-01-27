799f2f32ecef1281e4b9dcfa6294898383955552
Ryuichi Windows PC <ryuichi1com@gmail.com>
2013-01-27 23:19:44 +0900

    change sprite folder

--- config.rb
+++ config.rb
@@ -2,6 +2,5 @@ http_path = 'http://nonakaryuichi.github.com/cutandslice_me_sample/'
 sass_dir = 'scss'
 css_dir = 'css'
 images_dir = 'images'
-generated_images_dir = 'sprite'
 output_style = :nested
 line_comments = false
